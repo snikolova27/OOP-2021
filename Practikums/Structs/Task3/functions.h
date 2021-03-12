c4320200eb95419e3f337bbdf33008cf7bf813cf
Sonya Nikolova <snikolova27@gmail.com>
2021-03-12 16:30:53 +0200

    added some documentattion to Task 3

--- Practikums/Structs/Task3/functions.h
+++ Practikums/Structs/Task3/functions.h
@@ -1,26 +1,39 @@
+/**
+ * \ Structure working with a list of students and their graades
+ * \ author S. Nikolova
+  */
+
 #ifndef FUNC_H
 #define FUNC_H
 
-const int MAX_NUMBER_STUDENTS = 1000;
+const int MAX_NUMBER_STUDENTS = 1000;  //!< stores the maximum number of students in the list
 
 struct Grade
 {
-    char* Subject;
-    short grade;
+    char* Subject;  //!< stores the name of the subject
+    short grade;    //!< stores the grade for the subject entered
 
-    void getGrade();
+//! Function to get the grade for said subject
+    void getGrade();    
 };
 
 struct Student
 {
-    char* name;
-    char EGN [10];
-    char facultyNum [5];
-    Grade grades[5];
+    char* name;     //!< stores the 3 names of the student
+    char EGN [10];  //! stores the EGN of the student
+    char facultyNum [5];    //!< stores the faculty number of the student
+    Grade grades[5];        //!< stores the grades of the student
 
+//! Function to get the details about a student fro the console
     void readStudent();
+
+//! Function to capitalise all first letters of the 3 names of the student
     void formatname();
+
+//! Function to calculate the average grade of a student
     double avgGrade();
+
+//! Function to get the first and last name of a student
     char* firstLastName();
 };
 
