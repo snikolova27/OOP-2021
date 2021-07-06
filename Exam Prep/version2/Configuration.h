61447d77a9334744f752dec9ac84175053963202
Sonya Nikolova <snikolova27@gmail.com>
2021-07-06 11:46:56 +0300

    move deleted funcs to public

--- Exam Prep/version2/Configuration.h
+++ Exam Prep/version2/Configuration.h
@@ -8,11 +8,8 @@ private:
     std::vector<const Component *> components;
 
     Configuration() = default;
-    Configuration& operator = (const Configuration& other)  = delete;
-    Configuration(const Configuration& other) = delete;
 
-    void insert(Component* component);
-
+    void insert(Component *component);
 
 public:
     double price() const;
@@ -21,7 +18,10 @@ public:
     const Component &operator[](std::size_t index) const;
     friend std::ostream &operator<<(std::ostream &out, const Configuration &config);
 
-    friend Configuration* createConfiguration();
+    friend Configuration *createConfiguration();
+
+    Configuration &operator=(const Configuration &other) = delete;
+    Configuration(const Configuration &other) = delete;
 
     ~Configuration();
 };
